8b369a941651494ef7fdc53fa54a484d139b3a26
aTunass <nhatuan2022@gmail.com>
2022-12-24 15:06:16 +0700

    upload controller vong loai

--- Controller_vongloai_Tuan.py
+++ Controller_vongloai_Tuan.py
@@ -1,9 +1,6 @@
 import cv2
 def Controller(edges, PID, current_speed, current_angle):
-    line = 28
-    if (float(current_speed)<54):
-        line = 32
-    print('-----------------------------------', line)
+    line = 34 # doi 33 -> 34
     arr = []
     lineRow = edges[line,:]
     for x,y in enumerate(lineRow):
@@ -14,16 +11,16 @@ def Controller(edges, PID, current_speed, current_angle):
     center = int((arrmax + arrmin)/2)
     error = int(edges.shape[1]/2) - center
     #err duong xe dang ben phai va can sang trai nen de am de nguoc lai, goc duong la dg bi sang phai
-    angle = -PID(error, 0.31, 0.0001, 0.006)#0.3
-    if (float(current_speed)<45):
-            speed=150
+    angle = -PID(error, 0.34, 0.000, 0.065)#0.33 -> 0.34
+    if (float(current_speed)<46): #45 -> 46
+            speed=140 # 150 -> 140
     else: 
-        if (float(current_speed)>60):
+        if (float(current_speed)>50):
             speed = 0
-        else: speed = -10*abs((error)) + 230
-    cv2.circle(edges,(arrmin,line),5,(0,0,0),3)
-    cv2.circle(edges,(arrmax,line),5,(0,0,0),3)
-    cv2.line(edges,(center,line),(int(edges.shape[1]/2),edges.shape[0]),(0,0,0),3)
-    cv2.imshow("IMG", edges)
-    key = cv2.waitKey(1)
+        else: speed = -10*abs((error)) + 150
+    # cv2.circle(edges,(arrmin,line),5,(0,0,0),3)
+    # cv2.circle(edges,(arrmax,line),5,(0,0,0),3)
+    # cv2.line(edges,(center,line),(int(edges.shape[1]/2),edges.shape[0]),(0,0,0),3)
+    # cv2.imshow("IMG", edges)
+    # key = cv2.waitKey(1)
     return angle, speed
