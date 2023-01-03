c55cf56b4cb5776fd6570f2b5197d6abbec834de
aTunass <nhatuan2022@gmail.com>
2023-01-04 00:25:58 +0700

    update client and controller, upload for model

--- Controller_vongloai_Tuan.py
+++ Controller_vongloai_Tuan.py
@@ -1,42 +1,65 @@
 import cv2
-def Controller(edges, PID, current_speed, current_angle):
-    line = 32
+import time
+def Controller(edges, PID, current_speed, current_angle, check_err):
+    line = 26
     arr = []
     lineRow = edges[line,:]
     for x,y in enumerate(lineRow):
         if y==255:
             arr.append(x)
-
     if not arr:
-        return 25, 0
-
+        return float(current_angle), 100
     arrmax=max(arr)
     arrmin=min(arr)
-    center = int((arrmax + arrmin)/2)
-    if ((arrmax - arrmin)<60 and arrmin<60 and arrmax<110): #88 34
-        center = arrmin - 30
-    if ((arrmax - arrmin)<60 and arrmax>110 and arrmax>110):
-        center = arrmax + 30
-   
+    print(arrmax, arrmin)
+
+    if (check_err==1):
+        t = time.time()
+        while((time.time()-t)<0.13):
+            angle = 25
+            print('++++++++++++++++++++++++++++++++++++++')
+        check_err=0
+
+    if (check_err==2):
+        t = time.time()
+        while((time.time()-t)<0.13):
+            angle = -25
+            print('-------------------------------------------')
+        check_err=0
     
+    if (arrmax>150 and arrmin>125):
+        angle = 25
+        check_err=1
+        print('1111111111111111111111111111111')
+    if (arrmax<35 and arrmin<10):
+        angle = -25
+        check_err=2
+        print('2222222222222222222222222222222')
+
+    center = int((arrmax + arrmin)/2)
+    # print(arrmax, arrmin)
+    # if ((arrmax - arrmin)<60 and arrmin<60 and arrmax<110): #88 34
+    #     center = arrmin - 20
+    # if ((arrmax - arrmin)<60 and arrmax>110 and arrmax>110):
+    #     center = arrmax + 20
     
     error = int(edges.shape[1]/2) - center
     # print(error)
     #err duong xe dang ben phai va can sang trai nen de am de nguoc lai, goc duong la dg bi sang phai
-    angle = -PID(error, 0.34, 0.000, 0.065)#0.3
-    if (abs(error)<18):
-        set_speed = 51
-    else: set_speed = 47
-    # set_speed = 51
-    if (float(current_speed)<set_speed):
-        speed=150
-    else: 
-        if (float(current_speed)>60):
-            speed = 0
-        else: speed = -5.7*abs((error)) + 150
+    if (check_err==0):
+        angle = -PID(error, 0.35, 0.000, 0.065)#0.3
+        set_speed = 65 - abs(error)/5
+        # set_speed = 51
+        if (float(current_speed)<set_speed):
+            speed=150
+        else: 
+            if (float(current_speed)>65):
+                speed = 0
+            else: speed = -5*abs((error)) + 150
+    else: speed = 80
     # cv2.circle(edges,(arrmin,line),5,(0,0,0),3)
     # cv2.circle(edges,(arrmax,line),5,(0,0,0),3)
     # cv2.line(edges,(center,line),(int(edges.shape[1]/2),edges.shape[0]),(0,0,0),3)
     # cv2.imshow("IMG", edges)
     # key = cv2.waitKey(1)
-    return angle, speed
+    return angle, speed, check_err
--- client_vong_loai.py
+++ client_vong_loai.py
@@ -12,8 +12,9 @@ from PIL import Image
 import argparse
 from unet import UNet
 from Controller_vongloai_Tuan import Controller
+from utils_segment.data_loading import BasicDataset
 # from Controller_Uit_45 import Controller
-
+import logging
 # Create a socket object 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 
@@ -28,43 +29,65 @@ def get_args():
     parser = argparse.ArgumentParser(description='Predict masks from input images')
     parser.add_argument('--model', '-m', default='checkpoint_epoch300.pth', metavar='FILE',
                         help='Specify the file in which the model is stored')
+    parser.add_argument('--no-save', '-n', action='store_true', help='Do not save the output masks')
     parser.add_argument('--mask-threshold', '-t', type=float, default=0.5,
                         help='Minimum probability value to consider a mask pixel white')
     parser.add_argument('--scale', '-s', type=float, default=1.0,
                         help='Scale factor for the input images')
     parser.add_argument('--bilinear', action='store_true', default=False, help='Use bilinear upsampling')
+
     return parser.parse_args()
 def predict_img(net,
                 full_img,
                 device,
-                scale_factor=0.5,
-                out_threshold=0.5):
+                scale_factor,
+                out_threshold):
     net.eval()
-    img = torch.from_numpy(full_img)
-    img = img.transpose(1, 2).transpose(0, 1)
-    img = img / 255.0
-    img = img.unsqueeze(0).float()
+    img = torch.from_numpy(BasicDataset.preprocess(None, full_img, scale_factor, is_mask=False))
+    img = img.unsqueeze(0)
     img = img.to(device=device, dtype=torch.float32)
-    with torch.no_grad():
-        output = net(img)
-        probs = F.softmax(output, dim=1)[0]
-        tf = transforms.Compose([
-            transforms.ToPILImage(),
-            transforms.Resize((80, 160)),
-            transforms.ToTensor()
-        ])
-        full_mask = tf(probs.cpu()).squeeze()
-    return F.one_hot(full_mask.argmax(dim=0), net.n_classes).permute(2, 0, 1).numpy()
 
-def mask_to_image(mask: np.ndarray):
-    return Image.fromarray((np.argmax(mask, axis=0) * 255).astype(np.uint8))
+    with torch.no_grad():
+        output = net(img).cpu()
+        output = F.interpolate(output, (full_img.size[1], full_img.size[0]), mode='bilinear')
+        if net.n_classes > 1:
+            mask = output.argmax(dim=1)
+        else:
+            mask = torch.sigmoid(output) > out_threshold
+
+    return mask[0].long().squeeze().numpy()
+def mask_to_image(mask: np.ndarray, mask_values):
+    if isinstance(mask_values[0], list):
+        out = np.zeros((mask.shape[-2], mask.shape[-1], len(mask_values[0])), dtype=np.uint8)
+    elif mask_values == [0, 1]:
+        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=bool)
+    else:
+        out = np.zeros((mask.shape[-2], mask.shape[-1]), dtype=np.uint8)
+
+    if mask.ndim == 3:
+        mask = np.argmax(mask, axis=0)
+    for i, v in enumerate(mask_values):
+        out[mask == i] = v
+    return Image.fromarray(out)
 device = 'cuda'
 torch.cuda.is_available()
 """ Load the checkpoint """
 args = get_args()
-model = UNet(n_channels=3, n_classes=3, bilinear=args.bilinear)
-model = model.to(device)
-model.load_state_dict(torch.load(args.model, map_location=device))
+logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
+
+net = UNet(n_channels=3, n_classes=3, bilinear=args.bilinear)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+logging.info(f'Loading model {args.model}')
+logging.info(f'Using device {device}')
+
+net.to(device=device)
+state_dict = torch.load(args.model, map_location=device)
+mask_values = state_dict.pop('mask_values', [0, 1])
+net.load_state_dict(state_dict)
+
+logging.info('Model loaded!')
+
 sendBack_angle = 0
 sendBack_speed = 0
 def Control(angle, speed):
@@ -89,6 +112,7 @@ def remove_small_contours(image):
     image_remove = cv2.bitwise_and(image, image, mask=mask)
     return image_remove
 check = 50
+check_err=0
 line = 40
 angle = 0
 speed = 100
@@ -132,24 +156,34 @@ if __name__ == "__main__":
                 jpg_original = base64.b64decode(data_recv["Img"])
                 jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
                 imgage = cv2.imdecode(jpg_as_np, flags=1)
-                image = imgage[200:,:]
+                image = imgage[210:,:]
                 image_resize = cv2.resize(image, (160, 80))
                 image_resize = cv2.cvtColor(image_resize, cv2.COLOR_BGR2RGB)
+                img = Image.fromarray(image_resize)
                 # print(image_resize.shape)
 
                 """DETECT LANE"""
-                mask = predict_img(net=model,
-                           full_img=image_resize,
-                           device=device,
+                # mask = predict_img(net=net,
+                #            full_img=image_resize,
+                #            device=device,
+                #            scale_factor=args.scale,
+                #            out_threshold=args.mask_threshold,
+                #            )
+                # result = mask_to_image(mask)
+                # out = np.array(result)
+                mask = predict_img(net=net,
+                           full_img=img,
                            scale_factor=args.scale,
                            out_threshold=args.mask_threshold,
-                           )
-                result = mask_to_image(mask)
+                           device=device)
+                result = mask_to_image(mask, mask_values)
+                result = result.convert('L')
                 out = np.array(result)
+                # img_cv = cv2.cvtColor(out, cv2.COLOR_RGB2BGR)
                 img_remove = remove_small_contours(out)
-                edges = img_remove 
+                edges = img_remove
                 '''Controller'''
-                angle, speed = Controller(edges=edges, PID=PID, current_speed=current_speed, current_angle=current_angle)
+                angle, speed, check_err = Controller(edges=edges, PID=PID, current_speed=current_speed, current_angle=current_angle, check_err=check_err)
                 end = time.time()
                 fps = 1 / (end - start)
                 if (check==50):
@@ -157,6 +191,8 @@ if __name__ == "__main__":
                     check=0
                 check = check + 1
                 # print(fps)
+                # cv2.imshow("IMG", out)
+                # key = cv2.waitKey(1)
             except Exception as er:
                 print(er)
                 speed = -45
